1f253f04bf30901f84dd13d032b00c1b381d23e4
chrisguy <chris@chrisguy.co>
2019-07-02 14:35:59 +0100

    user model added

---
+++ models.py
@@ -0,0 +1,43 @@
+"""Journal models."""
+
+from flask_bcrypt import generate_password_hash
+from flask_login import UserMixin
+from peewee import *
+
+DATABASE = SqliteDatabase('journal.db')
+
+
+class User(UserMixin, Model):
+    """Define the User model."""
+
+    username = CharField(unique=True)
+    email = CharField(unique=True)
+    password = CharField(max_length=100)
+    joined_at = DateTimeField(default=datetime.datetime.now)
+    is_admin = BooleanField(default=False)
+
+    class Meta:
+        """Define the DB."""
+
+        database = DATABASE
+
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        """Create the user."""
+        try:
+            with DATABASE.transaction():
+                cls.create(
+                    username=username,
+                    email=email,
+                    password=generate_password_hash(password),
+                    is_admin=admin)
+        except IntegrityError:
+            raise ValueError("User already exists")
+
+
+def initalize():
+    """Initialize the DB."""
+    DATABASE.connect()
+    DATABASE.create_tables([User], safe=True)
+    DATABASE.close()
+
